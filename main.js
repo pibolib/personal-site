9d4030f62c219ed4442f16d1080767272f4d40bf
pibolib <pibolib@gmail.com>
2023-03-28 13:13:47 -0400

    added scrollbar when top part gets too long along with starting work on js part

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
 // TODO: write this
+
+class Work {
+    constructor(name, date, description, links, completionStatus) {
+        this.name = name; //a string
+        this.date = date; //a string
+        this.description = description; //a string
+        this.links = links; //an array of Link objects
+        this.completionStatus = completionStatus; //a boolean
+    }
+}
+
+class Link {
+    constructor(linkType, URL) {
+        this.URL = URL; //a string
+        this.linkType = linkType; //a string (see createLinkIcon for valid keys)
+    }
+}
+
+//takes a string
+function switchActiveSection(newSection) {
+    
+}
+
+//takes a string and a list of Work objects
+function createSection(name, contents) {
+
+}
+
+//takes a Link object
+function createLinkIcon(link) {
+
+}
