714ab8cbd2f7ac4f43258e62a5a1a8d4db4c67a6
pibolib <pibolib@gmail.com>
2023-03-28 21:20:58 -0400

    minor changes to main.css to fix issues, removed example nodes from index.html, added rendering functionality to main.js

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # personal-site
 [![Netlify Status](https://api.netlify.com/api/v1/badges/9c273381-87f8-4936-8da0-2e7aef55864f/deploy-status)](https://app.netlify.com/sites/pibosite/deploys)
-If this status is deployed, it means that the site is up, currently on new.pibolib.xyz. Will switch to www.pibolib.xyz when the time comes.
+If this status is deployed, it means that the site is up, currently on new.pibolib.xyz. This hosts the current *master* branch version of the site. Will switch to www.pibolib.xyz when the time comes.
 
 This is pibolib's personal site. Site implementation may differ from design to some degree.
 Built using HTML, CSS, and JS.
--- main.js
+++ main.js
@@ -25,7 +25,40 @@ function switchActiveSection(newSection) {
 
 //takes a string and a list of Work objects, returns a section container
 function createSection(name, contents) {
+    const container = document.createElement("div");
+    container.id = name;
+    container.classList.add("main");
+    for(var i = 0; i < contents.length; i++) {
+        const workContainer = createWorkContainer(contents[i]);
+        container.appendChild(workContainer);
+    }
+    return container;
+}
 
+function createWorkContainer(workJSON) {
+    const container = document.createElement("div");
+    container.classList.add("work");
+    const span1 = createSpan(workJSON.name, "work-title");
+    const span2 = createSpan(" ("+workJSON.date+")","work-date");
+    const br = document.createElement("br");
+    container.appendChild(span1);
+    span1.appendChild(span2);
+    container.appendChild(br);
+    const descriptionDiv = document.createElement("div");
+    const description = createSpan(workJSON.description, "en");
+    const descriptionJP = createSpan(workJSON.descriptionJP, "jp");
+    descriptionDiv.appendChild(description);
+    descriptionDiv.appendChild(descriptionJP);
+    container.appendChild(descriptionDiv);
+    const accentDiv = document.createElement("div");
+    accentDiv.classList.add("work-accent");
+    if (workJSON.completionStatus) {
+        accentDiv.classList.add("finished");
+    } else {
+        accentDiv.classList.add("unfinished");
+    }
+    container.appendChild(accentDiv);
+    return container;
 }
 
 //takes a Link object, returns a link container containing an image with an a attached
@@ -33,11 +66,47 @@ function createLinkIcon(link) {
 
 }
 
+function createSpan(text, classes) {
+    const span = document.createElement("span");
+    const textNode = document.createTextNode(text);
+    span.classList.add(classes);
+    span.appendChild(textNode);
+    return span;
+}
+
 const worksGames = [
-    Work("???","TBD","More information to come soon...","...?",[],false),
-    Work("TSUNAGARI TOWER: Phoebe's Ascent","2022","A puzzle game developed in Godot 3.5. Solo developed by pibolib, originally for GMTK Game Jam 2022.","Godot 3.5で開発されたパズルゲームです。自作ゲームです。元GMTK Game Jam 2022のゲームでした。",[Link("github","https://github.com/pibolib/tsunagari-tower"),Link("itchio","https://pibolib.itch.io/tsunagari-tower")], true),
-    Work("Trung Sisters","2022","A strategy game developed in Godot 3.4. Developed with my brother for Historically Accurate Game Jam 5.","Godot 3.4で開発されたストラテジーゲームです。Historically Accurate Game Jamのためのゲームです。リードプログラマーの役割でした。",[Link("github","https://github.com/pibolib/trung-sisters"),Link("itchio","https://pibolib.itch.io/trung-sisters")], true),
-    Work("GemStreak: Curse of the Time Machine","2021","A puzzle game developed in Godot 3.2. Developed with friends for Godot Wild Jam #35.","Godot 3.2で開発されたパズルゲームです。Godot Wild Jam #35のためのゲームでした。リードデザイナーとリードプログラマーの役割でした。",[Link("github","https://github.com/pibolib/gemstreak"),Link("itchio","https://pibolib.itch.io/gemstreak")], true),
+    {
+        name: "???",
+        date: "TBD",
+        description: "In progress. More information to come soon.",
+        descriptionJP: "開発中です。詳しくはまだ決まっていません。",
+        links: [],
+        completionStatus: false
+    },
+    {
+        name: "TSUNAGARI TOWER: Phoebe's Ascent",
+        date: "2022-23",
+        description: "A puzzle game developed in Godot 3.5. Solo developed by pibolib, originally for GMTK Game Jam 2022.",
+        descriptionJP: "Godot 3.5で開発されたパズルゲームです。自作ゲームです。元GMTK Game Jam 2022のゲームでした。",
+        links: [new Link("github","https://github.com/pibolib/tsunagari-tower"), new Link("itchio","https://pibolib.itch.io/tsunagari-tower")],
+        completionStatus: true
+    },
+    {
+        name: "Trung Sisters",
+        date: "2022",
+        description: "A strategy game developed in Godot 3.4. Developed with my brother for Historically Accurate Game Jam 5.",
+        descriptionJP:"Godot 3.4で開発されたストラテジーゲームです。Historically Accurate Game Jamのためのゲームです。リードプログラマーでした。",
+        links: [new Link("github","https://github.com/pibolib/trung-sisters"), new Link("itchio","https://pibolib.itch.io/trung-sisters")],
+        completionStatus: true
+    },
+    {
+        name: "GemStreak: Curse of the Time Machine",
+        date: "2021",
+        description: "A puzzle game developed in Godot 3.2. Developed with friends for Godot Wild Jam #35.",
+        descriptionJP: "Godot 3.2で開発されたパズルゲームです。Godot Wild Jam #35のためのゲームでした。リードデザイナーとリードプログラマーでした。",
+        links: [new Link("github","https://github.com/pibolib/gemstreak"), new Link("itchio","https://pibolib.itch.io/gemstreak")],
+        completionStatus: true
+    }
 ];
 
 const worksMusic = [
@@ -47,3 +116,7 @@ const worksMusic = [
 const worksOther = [
 
 ];
+
+const worksGamesContainer = createSection("games", worksGames);
+const home = document.getElementById("home");
+home.appendChild(worksGamesContainer);
